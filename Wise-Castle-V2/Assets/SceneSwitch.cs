baa02ab65fad7e2a980eb8d145851f2f17688c99
Ryan <mayerryan98@gmail.com>
2020-02-13 18:22:34 -0500

    scene switching
    game scenes not done

--- Wise-Castle-V2/Assets/SceneSwitch.cs
+++ Wise-Castle-V2/Assets/SceneSwitch.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+	public void LoadMainScene(){
+		SceneManager.LoadScene("main");
+	}
+	
+    public void LoadArtScene(){
+		SceneManager.LoadScene("art");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void LoadEnglishScene(){
+		SceneManager.LoadScene("english");
     }
+
+	public void LoadChemScene(){
+		SceneManager.LoadScene("chemistry");
+	}
+	
+	public void LoadMathScene(){
+		SceneManager.LoadScene("math");
+	}
+	
+	public void LoadPlayerScene(){
+		SceneManager.LoadScene("player");
+	}
+	
+	public void LoadShopScene(){
+		SceneManager.LoadScene("shop");
+	}
+	
+	public void LoadArtTutorial(){
+		SceneManager.LoadScene("tutorial_art");
+	}
+	
+	public void LoadChemTutorial(){
+		SceneManager.LoadScene("tutorial_chem");
+	}
+	
+	public void LoadEnglishTutorial(){
+		SceneManager.LoadScene("tutorial_english");
+	}
+	
+	public void LoadMathTutorial(){
+		SceneManager.LoadScene("tutorial_math");
+	}
 }
