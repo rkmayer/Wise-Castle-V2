0be9c7ceb6afbc5a087782688490ee9145c8d423
Ryan <mayerryan98@gmail.com>
2020-02-20 16:28:09 -0500

    more stuff for math game

---
+++ Wise-Castle-V2/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+		Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+		transform.position = transform.position + horizontal * 150 * Time.deltaTime;
+        
+    }
+}
