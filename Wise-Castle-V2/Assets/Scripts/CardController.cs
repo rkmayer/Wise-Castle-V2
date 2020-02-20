f52ca065107c70f6f08bb11d780d8d43d15b557f
jkelsii <60824698+jkelsii@users.noreply.github.com>
2020-02-19 21:46:12 -0500

    Card Prefab + Controller
    finished the prefab (hope it works out) + added a controller script for the cards

--- Wise-Castle-V2/Assets/Scripts/CardController.cs
+++ Wise-Castle-V2/Assets/Scripts/CardController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class CardController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private string _cardID; //the card's value (for matching)
+    private string cardText; //the card's text value (what is on the card)
+    
+    private bool isFlipped = false; //the card's current position (back or front of card showing)
+    
+    private Sprite backOfCard; //sprite for back of card
+    [SerializeField]
+    private Sprite frontOfCard; //sprite for front of card
+    
+    private SpriteRenderer spriteRenderer;
+    
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        backOfCard = spriteRenderer.sprite;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public string cardID
     {
-        
+        get {
+            return _cardID;
+        }
+        set
+        {
+            _cardID = value;
+        }
+    }
+    
+    private void OnMouseDown()
+    {
+        if(!isFlipped)
+        {
+            FlipCard();
+        }
+    }
+    
+    void FlipCard()
+    {
+        if(!isFlipped)
+        {
+            spriteRenderer.sprite = frontOfCard;
+            isFlipped = true;
+        }
+        else
+        {
+            spriteRenderer.sprite = backOfCard;
+            isFlipped = false;
+        }
     }
 }
