2e8a8f4446a58256fdca9e868475a6089b79b837
Ryan <mayerryan98@gmail.com>
2020-03-13 13:44:16 -0400

    change difficulty almost working

--- Wise-Castle-V2/Assets/Scripts/DifficultyScript.cs
+++ Wise-Castle-V2/Assets/Scripts/DifficultyScript.cs
@@ -8,6 +8,7 @@ Ryan Mayer
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DifficultyScript : MonoBehaviour
 {
@@ -17,15 +18,30 @@ public class DifficultyScript : MonoBehaviour
 	easy = 0; normal = 1; hard = 2;
 	*/
 	
+	[SerializeField] Button easyBtn, normalBtn, hardBtn;
+	
+	public void Start(){
+		
+	}
+	
 	public void DifficultySetEasy(){
 		PlayerPrefs.SetInt("Difficulty", 0);
+		easyBtn.interactable = false;
+		normalBtn.interactable = true;
+		hardBtn.interactable = true;
 	}
 	
 	public void DifficultySetNormal(){
 		PlayerPrefs.SetInt("Difficulty", 1);
+		easyBtn.interactable = true;
+		normalBtn.interactable = false;
+		hardBtn.interactable = true;
 	}
 	
 	public void DifficultySetHard(){
 		PlayerPrefs.SetInt("Difficulty", 2);
+		easyBtn.interactable = true;
+		normalBtn.interactable = true;
+		hardBtn.interactable = false;
 	}
 }
